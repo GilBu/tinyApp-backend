865ab9f1af7e90dbf5080737cc43eb1bc4d071c1
gilbu <gilbertbuix57@gmail.com>
2021-04-08 11:47:59 -0700

    build out question model and connect to mongo

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+const db = require('./config/keys').mongoURI
 
+mongoose 
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongodb connected'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("Hello World"))
 
